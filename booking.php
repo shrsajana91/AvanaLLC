fadf92e1cf3e7affa5ef5c6f76a85311f1928103
sajana Shrestha <shrsajana91@gmail.com>
2019-02-17 17:00:46 -0600

    refactoring index.php

--- booking.php
+++ booking.php
@@ -1,4 +1,4 @@
-
+l
 <!-- Booking Section for another page -->
 
 <div class="container">
@@ -10,40 +10,60 @@
         <div class="col s12">
             <h3 class="green"> Select Time</h3>
         </div>
-        <div class="col s6">
-            <a href="#" class="btn-large waves-effect waves-light">
-                <span> 15 Minutes </span>
-                <i class="material-icons right">access_time</i>
-            </a>
-            <div class="col s6">
-                <a href="#" class="btn-large wave-effect waves-light">
-                    <span> 30 minutes </span>
-                    <i class="material-icons right">access_time</i>
-                </a>
-            </div>
+        <div class="input-field col s12">
+            <select >
+                <option value="" disabled selected> Select Time</option>
+                <option value="1"> 15 minutes</option>
+                <option value="2">30 minutes</option>
+
+            </select>
+            <label>Select any</label>
+
         </div>
+
 
         <div class="col s12">
 
-            <h3 class="green"> Select Quantity </h3>
+            <h3 class="green"> Select Rides </h3>
         </div>
-        <div class="col s12">
-            <a href="#" class="btn-floating blue pulse">
-                <i class="material-icons"> add </i>
-            </a>
 
-            <a href="#" class="btn-floating red">
-                <i class="material-icons">remove</i>
-            </a>
+        <div class="input-field col s12">
+            <select class ="icons">
+                <option value="" disabled selected> Select Ride</option>
+                <option value="" data-icon ="image/pig.jpg"> Pig</option>
+                <option value=""  data-icon ="image/pig.jpg">Tiger</option>
+                <option value="" data-icon ="image/pig.jpg"> Elephant</option>
+            </select>
+            <label>Select any</label>
+
         </div>
+
+
         <div class="col s12">
 
             <h3 class="green"> Total</h3>
+
         </div>
 
 
     </div>
 </div>
 
+<!-- jquery Link -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
+
+<script>
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
+</script>
 
 
