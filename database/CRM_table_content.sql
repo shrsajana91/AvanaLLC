34a8ccface4513f8a819df709d54eb86cfee1c49
Nabin Shrestha <nabin5s@hotmail.com>
2019-02-17 22:08:48 -0600

    Changing table structure for content

--- database/CRM_table_content.sql
+++ database/CRM_table_content.sql
@@ -7,5 +7,6 @@
 
 CREATE TABLE `content` (
   `content_id` int(11) NOT NULL,
+  `picture` varchar(64),
   `wording` text COLLATE utf32_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
