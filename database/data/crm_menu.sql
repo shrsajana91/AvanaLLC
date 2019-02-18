1e6be9f9f8ed69f27abffddfaf73a4211adcecba
Nabin Shrestha <nabin5s@hotmail.com>
2019-02-17 21:54:32 -0600

    Storing data from db to git

---
+++ database/data/crm_menu.sql
@@ -0,0 +1,4 @@
+INSERT INTO crm.menu (menu_id, url, name) VALUES (1, 'https://google.com', 'Home');
+INSERT INTO crm.menu (menu_id, url, name) VALUES (2, '#zuride', 'ZuRide');
+INSERT INTO crm.menu (menu_id, url, name) VALUES (3, '#', 'Bunjee');
+INSERT INTO crm.menu (menu_id, url, name) VALUES (4, '#', 'Login');
