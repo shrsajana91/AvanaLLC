5e0b30360ce1c901b5b290ef744e7dcf7df22b37
sajana Shrestha <shrsajana91@gmail.com>
2019-02-18 20:44:01 -0600

    product table data dump

--- database/data/crm_product.sql
+++ database/data/crm_product.sql
@@ -1,4 +1,4 @@
-INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (1, 'Tiger', 'image/pig.jpg', 2, 5);
+INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (1, 'Tiger', 'image/tiger.jpg', 2, 5);
 INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (2, 'Pig', 'image/pig.jpg', 2, 5);
-INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (3, 'Lion', 'image/pig.jpg', 2, 5);
-INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (4, 'Elephant', 'image/pig.jpg', 5, 5);
+INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (3, 'Lion', 'image/lion.jpeg', 2, 5);
+INSERT INTO crm.product (product_id, name, picture, quantity, price) VALUES (4, 'Elephant', 'image/elephant.jpg', 5, 5);
