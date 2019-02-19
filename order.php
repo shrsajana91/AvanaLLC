c21d09225da89ffb3fb79ec9c83ed6ea9a8090cc
sajana Shrestha <shrsajana91@gmail.com>
2019-02-18 22:09:05 -0600

    displayed form data in order page

--- order.php
+++ order.php
@@ -1,10 +1,29 @@
 <?php
-echo "This is Order Page";
+
 include('db.php');
 include('header.php');
-var_dump($_POST);
+
 $bookingDuration=$_POST['duration'];
 $bookingRide=$_POST['ride'];
-echo $bookingDuration . " " . $bookingRide;
 
+
+$sql =$connection->prepare('SELECT * FROM product WHERE name="' . $bookingRide . '"');
+$sql->execute();
+
+$result =$sql->setFetchMode(PDO::FETCH_ASSOC);
+$rideInfo =$sql->fetchAll();
+$rideInfo = $rideInfo[0];
+
+?>
+
+<h1>We have received your order. </h1>
+
+<p> You ordered: <?php echo $rideInfo['name']; ?> </p>
+<p> Duration: <?php echo $bookingDuration; ?> minutes</p>
+<p> Price: $<?php echo $rideInfo['price']; ?></p>
+<p> Total: $<?php echo ($rideInfo['price']/15)*$bookingDuration;?></p>
+
+
+<?php
 include('footer.php');
+?>
